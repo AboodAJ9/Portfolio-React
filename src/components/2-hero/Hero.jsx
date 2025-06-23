c42be98a4f3722fd7e441fdcd8d27f9fd331bb82
Abdallah Jaber <abdallah.jaber@student.hs-rm.de>
2025-06-23 23:47:32 +0200

    feat: prevent image copying via context menu and drag disable

--- src/components/2-hero/Hero.jsx
+++ src/components/2-hero/Hero.jsx
@@ -6,12 +6,11 @@ import { motion } from 'framer-motion';
 import { useTranslation } from 'react-i18next';
 
 const Hero = () => {
-    const lottieRef = useRef(); 
+    const lottieRef = useRef();
     const { t, i18n } = useTranslation();
     console.log("dfdfd" + lottieRef.current)
     useEffect(() => {
         if (lottieRef.current) {
-            console.log("heeeeee"); 
             // @ts-ignore
             //https://lottiereact.com/
             lottieRef.current.setSpeed(0.5);
@@ -22,44 +21,48 @@ const Hero = () => {
             <div className="left-section">
 
                 <div className="parent-avatar flex">
-                    <motion.img 
-                    initial={{transform: "scale(0)" }}
-                    animate={{transform: "scale(1.1)" }}
-                    transition={{damping:6, type: "spring", stiffness: 100}}
-                    src="./ownImage_cycle.png" className="avatar" alt="" />
+                    <motion.img
+                        initial={{ transform: "scale(0)" }}
+                        animate={{ transform: "scale(1.1)" }}
+                        transition={{ damping: 6, type: "spring", stiffness: 100 }}
+                        src="./ownImage_cycle.png" className="avatar" alt="private"
+                        onContextMenu={(e) => e.preventDefault()}
+                        draggable={false}
+                        style={{ userSelect: "none", pointerEvents: "none" }} />
                     <div className='icon-verified'> </div>
                 </div>
+
 
                 <h1 className='title'>
                     {t("welcome")}
                 </h1>
 
-                <p className='sub-title'> 
+                <p className='sub-title'>
                     {t("intro")}
                 </p>
 
-                <div className='all-icons flex'> 
-                    <a href ="https://github.com/AboodAJ9" className="icon icon-github"></a>
-                    <a href ="https://www.linkedin.com/in/abdallah-jaber-hsrm/" className="icon icon-linkedin"></a>
-                    <a href ="https://www.codewars.com/users/aboodAJ_991" className="icon icon-codewars"></a>
+                <div className='all-icons flex'>
+                    <a href="https://github.com/AboodAJ9" className="icon icon-github"></a>
+                    <a href="https://www.linkedin.com/in/abdallah-jaber-hsrm/" className="icon icon-linkedin"></a>
+                    <a href="https://www.codewars.com/users/aboodAJ_991" className="icon icon-codewars"></a>
                 </div>
 
 
             </div>
-            <div className="right-section animation">  
+            <div className="right-section animation">
 
                 <Lottie
-                lottieRef={lottieRef}
-                className='mail-animation' 
-                style = {{height: "500px"}} 
-                animationData={devAnimation} /> 
+                    lottieRef={lottieRef}
+                    className='mail-animation'
+                    style={{ height: "500px" }}
+                    animationData={devAnimation} />
 
             </div>
         </section>
     );
 }
 
-export default Hero; 
+export default Hero;
 
 
 //npm install lottie-react
