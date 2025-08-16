a921613da147967d4db2f8f88b563af3ae666206
Abdallah Jaber <abdallah.jaber@student.hs-rm.de>
2025-08-16 16:24:09 +0200

    feat(footer): add anchor links to scroll to sections

--- src/components/7-footer/Footer.jsx
+++ src/components/7-footer/Footer.jsx
@@ -4,10 +4,10 @@ const Footer = () => {
     return (
         <footer className='flex'> 
             <ul className='flex'>
-                <li><a href="">About</a></li>
-                <li><a href="">Projects</a></li>
-                <li><a href="">Skills</a></li>
-                <li><a href="">Timeline</a></li>
+                <li><a href="#up">About</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#skills">Skills</a></li>
+                <li><a href="#timeline">Timeline</a></li>
             </ul>
 
             <p>© 2025 Abdallah Jaber.</p>
